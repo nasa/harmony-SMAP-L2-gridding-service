bfe8e9eb000778c22da7ef33a1c04318d9e0a900
Matt Savoie <matthew.savoie@colorado.edu>
2024-12-02 13:17:17 -0700

    DAS-2255: Upate link pattern in the extract-release-notes file.

--- bin/extract-release-notes.sh
+++ bin/extract-release-notes.sh
@@ -16,7 +16,7 @@ VERSION_PATTERN="^## [\[]v"
 ## captures url links
 ## [unreleased]:https://github.com/nasa/harmony-browse-image-generator/compare/1.2.0..HEAD
 ## [v1.2.0]: https://github.com/nasa/harmony-browse-image-generator/compare/1.1.0..1.2.0
-LINK_PATTERN="^\[.*\].*\.\..*"
+LINK_PATTERN="^\[.*\].*releases/tag/.*"
 
 # Read the file and extract text between the first two occurrences of the
 # VERSION_PATTERN
